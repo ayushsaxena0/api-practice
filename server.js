1a72e63ff3861bd4fd8cd2eadfbda0f0db788197
Ayush <ayushsaxena917@gmail.com>
2025-01-31 14:56:58 +0530

    working demo api successfull

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+const pokemon = {
+  bulbasaur: {
+    type: "Grass, Poison",
+    evolution: {
+      ivysaur: "At level 16",
+      Venusaur: "At level 32",
+    },
+    "pokedex number": "#0001",
+  },
+  charmander: {
+    type: "Fire",
+    evolution: {
+      charmeleon: "At level 16",
+      charizard: "At level 36",
+    },
+    "pokedex number": "#0004",
+  },
+  squirtle: {
+    type: "Water",
+    evolution: {
+      wartortle: "At level 16",
+      blastoise: "At level 36",
+    },
+    "pokedex number": "#0007",
+  },
+  unknown: {
+    type: "unknown",
+    evolution: "unknown",
+    "pokedex number": "#unknown",
+  },
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api/:pokemon", (req, res) => {
+  const pokemonName = req.params.pokemon.toLowerCase();
+  if (pokemon[pokemonName]) {
+    res.json(pokemon[pokemonName]);
+  } else {
+    res.json(pokemon["unknown"]);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running. Listening on port ${PORT}`);
+});
